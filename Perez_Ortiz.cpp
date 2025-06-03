efca89742e249c2af692fa547f7f4766655a5114
Luis A <ruisu_3912@hotmail.com>
2025-06-02 20:46:18 -0600

    cambios minimos

--- Perez_Ortiz.cpp
+++ Perez_Ortiz.cpp
@@ -6,43 +6,19 @@
 #include <string.h>
 
 int main(void) {
-	FILE *archivo;
-	archivo = fopen("carta.txt", "r");
-
-	if (archivo == NULL) {
-		printf("Error: archivo no encontrado X_X!\n");
-		return 1;
+	char url[1000];
+	char urlSegura[10] = "https";
+	printf("ingresa el link a verificar:\n");
+	scanf("%s", &url);
+	char *esSegura = strstr(url, urlSegura);
+	if (esSegura != NULL)
+	{
+		printf("Date bro el sitio es seguro");
 	}
-
-	char info_carta[1000];
-	char delimitadorsignos[] = ",;:. \n!\"'";
-	char palabras[1000];
-	int contador = 0;
-
-	while (fgets(info_carta, sizeof(info_carta), archivo)) {
-		char *delimitador = strtok(info_carta, delimitadorsignos);
-		while (delimitador != NULL) {
-			//strcpy(info_carta, palabras);
-			contador++;
-			delimitador = strtok(NULL, " ");
-		}
+	else
+	{
+		printf("Mucho ojo el sitio no es seguro");
 	}
-	printf("Numero de palabras: %d\n", contador);
-	// int *repDePalabras;
-	// repDePalabras = (int *) calloc(contador, sizeof(int));
-	// if (repDePalabras == NULL)
-	// {
-	// 	printf("¡No se pudo reservar memoria!\n");
-	// }
-	// else
-	// {
-	// 	if (true)
-	// 	{
-	// 		printf("");
-	// 	}
-	// }
-
-	fclose(archivo);
 	return 0;
 }
 
